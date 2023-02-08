270f4561d0645ad9a0e8a0c3e15f9a5f81d7af55
wen-wong <wen.wong@mail.mcgill.ca>
2023-02-07 22:15:43 -0500

    Remove boilerplate code

---
+++ src/views/SearchView.vue
@@ -0,0 +1,92 @@
+<script>
+import axios from "axios";
+import MovieCard from "../components/MovieCard.vue";
+
+export default {
+	components: {
+		MovieCard
+	},
+	data() {
+		return {
+			movieList: [],
+			isMovieList: [false],
+			search: ""
+		};
+	},
+	methods: {
+		async fetchMovies() {
+			const movieResponse = await axios.get(
+				"https://api.themoviedb.org/3/discover/movie?api_key=f4a943efca00a3cd96ac56ff8ad1ea3c"
+			);
+			this.movieList = movieResponse.data.results;
+		},
+		async searchMovies(event) {
+			const movieResponse = await axios.get(
+				`https://api.themoviedb.org/3/search/movie?api_key=f4a943efca00a3cd96ac56ff8ad1ea3c&query=${event.target.value}`
+			);
+			this.movieList = movieResponse.data.results;
+		},
+		routeToMovie(id) {
+			// TODO add route to movie
+		}
+	},
+	async mounted() {
+		await this.fetchMovies();
+	}
+};
+</script>
+
+<template>
+	<div class="search">
+		<div class="search-bar">
+			<label for="movie-search">Search for a movie</label>
+			<input
+				type="search"
+				id="movie-search"
+				name="movie-search"
+				placeholder="Search for your favorite movie"
+				@input="searchMovies"
+			/>
+		</div>
+		<div class="movie-container">
+			<MovieCard
+				v-for="movie in movieList"
+				:title="movie.title"
+				:description="movie.overview"
+				@click="routeToMovie(movie.id)"
+			/>
+		</div>
+	</div>
+</template>
+
+<style>
+.search {
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+}
+
+.search-bar {
+	display: flex;
+	flex-direction: column;
+}
+
+.movie-container {
+	margin: 1rem 0rem;
+}
+
+label {
+	font-size: 1rem;
+	font-weight: bold;
+	line-height: 2rem;
+}
+
+#movie-search {
+	font-size: 1.25rem;
+	width: 100vh;
+	padding: 0.75rem;
+	border: 0.01rem solid grey;
+	border-radius: 0.5rem;
+}
+</style>
