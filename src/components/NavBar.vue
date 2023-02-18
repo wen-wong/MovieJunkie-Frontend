8bfb779de90d795ac87124eda87f7ca054e4bb42
wen-wong <wen.wong@mail.mcgill.ca>
2023-02-18 18:19:49 -0500

    Create NavBar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+	<div class="nav-container">
+		<div class="nav-item-container">
+			<img src="../assets/icons/camera_roll_30px.svg" alt="moviejunkie-logo" />
+			<div class="nav-item item-left">Movie Junkie</div>
+		</div>
+		<div class="nav-item-container">
+			<div class="nav-item item-right">Account</div>
+			<img src="../assets/icons/exit_to_app_30px.svg" alt="moviejunkie-logo" />
+		</div>
+	</div>
+</template>
+
+<style>
+.nav-container {
+	display: flex;
+	flex-direction: row;
+	justify-content: space-between;
+	margin: 2rem 0rem;
+	padding: 1rem 4rem;
+	font-size: 1.25rem;
+}
+
+.nav-item-container {
+	display: flex;
+	flex-direction: row;
+}
+
+.nav-item {
+	font-weight: bold;
+}
+
+.item-left {
+	margin-left: 0.5rem;
+}
+
+.item-right {
+	margin-right: 0.5rem;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,16 @@
-<script setup></script>
+<script setup>
+import NavBar from "../components/NavBar.vue";
+</script>
 
 <template>
-	<main></main>
+	<div class="home-container">
+		<NavBar />
+	</div>
 </template>
+
+<style>
+.home-container {
+	height: 100vh;
+	width: 100%;
+}
+</style>
