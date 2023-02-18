0ae4d8c1ae71d4a1a29a2575499bc3179038cb77
Matthew Litwiller <matthew.litwiller@mail.mcgill.ca>
2023-02-17 19:53:00 -0500

    created AccountInformationView page and added skeleton code

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "../views/HomeView.vue";
 import SearchView from "../views/SearchView.vue";
 import MovieView from "../views/MovieView.vue";
 import ErrorView from "../views/ErrorView.vue";
+import AccountInformationView from "../views/AccountInformationView.vue";
 
 const router = createRouter({
 	history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
 			path: "/:catchAll(.*)",
 			name: "error",
 			component: ErrorView
+		},
+		{
+			path: "/account/:username",
+			name: "account",
+			component: AccountInformationView
 		}
 	]
 });
---
+++ src/views/AccountInformationView.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="user-account">
+    <img class="user-account__photo" :src="photoUrl" alt="User Photo">
+    <div class="user-account__info">
+      <h2 class="user-account__name">{{ firstname }} {{ lastname }}</h2>
+      <p class="user-account__email">{{ email }}</p>
+      <button class="user-account__edit-btn" @click="editAccount">Edit Account</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AccountInformationView",
+  data(){
+    return {
+      firstname: "John",
+      lastname: "Doe",
+      username: "johndooooe",
+      email: "jdoe@gmail.com",
+      password: "jdjd"
+    }
+  },
+
+  props: {
+    photoUrl: {
+      type: String,
+      default: 'https://via.placeholder.com/150', // default stock photo
+    }
+  },
+
+  methods: {
+    editAccount() {
+      console.log('you are now editing your account');
+    },
+  },
+};
+</script>
+
+<style>
+
+.user-account {
+  display: flex;
+  align-items: center;
+  justify-content: center; /* center horizontally */
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+}
+.user-account__photo {
+  border-radius: 50%;
+  width: 150px;
+  height: 150px;
+  margin-right: 1rem;
+}
+.user-account__info {
+  flex: 1;
+}
+.user-account__name {
+  font-size: 2rem;
+  margin: 0;
+}
+.user-account__email {
+  margin: 0;
+  color: #888;
+}
+.user-account__edit-btn {
+  margin-top: 1rem;
+  padding: 0.5rem 1rem;
+  border: none;
+  background-color: #0074d9;
+  color: #fff;
+  border-radius: 4px;
+  cursor: pointer;
+}
+</style>
