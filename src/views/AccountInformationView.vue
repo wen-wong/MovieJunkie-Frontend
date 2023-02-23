f5d3aba07d3c2d27b39584627064e45d344db8c9
Matthew Litwiller <matthew.litwiller@mail.mcgill.ca>
2023-02-23 17:57:28 -0500

    removed useless data in AccountInformationView

--- src/views/AccountInformationView.vue
+++ src/views/AccountInformationView.vue
@@ -10,15 +10,6 @@ import NavBar from "@/components/NavBar.vue";
 export default {
   name: "AccountInformationView",
   components: {NavBar},
-  data(){
-    return {
-      firstname: "John",
-      lastname: "Doe",
-      username: "johndooooe",
-      email: "jdoe@gmail.com",
-      password: "jdjd",
-    }
-  },
 
   props: {
     photoUrl: {
