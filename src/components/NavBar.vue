68456f867bdc970f9f070e3a156c770eabfdb454
wen-wong <wen.wong@mail.mcgill.ca>
2023-02-22 20:44:09 -0500

    Shift account icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,89 +1,112 @@
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 import AccountDropdown from "@/components/AccountDropdown.vue";
 export default {
+	components: {
+		AccountDropdown
+	},
+	data() {
+		return {
+			showModal: false,
+			edit: false,
+			del: false,
+			username: "",
+			password: "",
+			email: ""
+		};
+	},
+	methods: {
+		handleOptionSelected(option) {
+			if (option == "Edit Account") {
+				//add logic to redirect to popup (once popup is implemented)
+				this.showModal = true;
+				this.edit = true;
+				this.del = false;
+				console.log("Redirecting to edit account popup");
+			}
+			if (option == "Delete Account") {
+				//add logic to redirect to popup (once popup is implemented)
+				this.showModal = true;
+				this.del = true;
+				this.edit = false;
+				console.log("Redirecting to delete account popup");
+			}
+		},
 
-  components: {
-    AccountDropdown,
-  },
-  data() {
-    return {
-      "showModal": false,
-      "edit": false,
-      "del": false,
-      "username": "",
-      "password": "",
-      "email": "",
-    }
-  },
-  methods: {
-    handleOptionSelected(option) {
-      if(option == "Edit Account"){
-        //add logic to redirect to popup (once popup is implemented)
-        this.showModal = true;
-        this.edit = true;
-        this.del = false;
-        console.log("Redirecting to edit account popup")
-      }
-      if(option == "Delete Account"){
-        //add logic to redirect to popup (once popup is implemented)
-        this.showModal = true;
-        this.del = true;
-        this.edit = false;
-        console.log("Redirecting to delete account popup")
-      }
-    },
+		editAccount(username, email, password) {
+			console.log(
+				"Editing account with username, email, password:" +
+					username +
+					" " +
+					email +
+					" " +
+					password
+			);
+			this.edit = false;
+			this.showModal = false;
+			axios
+				.post("http://localhost:8080/account/edit/", {
+					username: username,
+					password: password,
+					email: email
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		},
 
-    editAccount(username, email, password){
-      console.log("Editing account with username, email, password:" + username + " " + email + " " + password);
-      this.edit = false;
-      this.showModal = false;
-      axios.post('http://localhost:8080/account/edit/', {
-        username: username,
-        password: password,
-        email: email
-      })
-          .catch(error => {
-            console.log(error)
-          })
-    },
+		deleteAccount(username, password) {
+			console.log(
+				"Deleting account with username and password: " + username + " " + password
+			);
+			this.del = false;
+			this.showModal = false;
+			axios
+				.post("http://localhost:8080/account/delete/", {
+					username: username,
+					password: password
+				})
+				.catch((error) => {
+					console.log(error);
+				});
+		},
 
-    deleteAccount(username, password){
-      console.log("Deleting account with username and password: " + username + " " + password)
-      this.del = false;
-      this.showModal = false;
-      axios.post('http://localhost:8080/account/delete/', {
-        username: username,
-        password: password
-      })
-          .catch(error => {
-            console.log(error)
-          })
-    },
+		logout() {
+			console.log("Logging out");
+		},
 
-    logout(){
-      console.log("Logging out")
-    },
-
-    returnHome(){
-      console.log("Returning to home page")
-    }
-  },
+		returnHome() {
+			console.log("Returning to home page");
+		}
+	}
 };
 </script>
 
 <template>
 	<div class="nav-container">
 		<div class="nav-item-container">
-			<img class="nav-item nav-icon" src="../assets/icons/camera_roll_30px.svg" alt="moviejunkie-logo" @click="returnHome"/>
+			<img
+				class="nav-item nav-icon"
+				src="../assets/icons/camera_roll_30px.svg"
+				alt="moviejunkie-logo"
+				@click="returnHome"
+			/>
 			<div class="nav-item item-left" @click="returnHome">Movie Junkie</div>
 		</div>
 		<div class="nav-item-container">
-<!--			<button class="nav-item item-right">Account-->
-        <AccountDropdown :options="['Edit Account', 'Delete Account']" @optionSelected="handleOptionSelected" />
-<!--      </button>-->
-			<img class ="nav-item nav-icon" src="../assets/icons/exit_to_app_30px.svg" alt="moviejunkie-logo" @click="logout"/>
+			<!--			<button class="nav-item item-right">Account-->
+			<AccountDropdown
+				:options="['Edit Account', 'Delete Account']"
+				@optionSelected="handleOptionSelected"
+			/>
+			<!--      </button>-->
+			<img
+				class="nav-item nav-icon"
+				src="../assets/icons/exit_to_app_30px.svg"
+				alt="moviejunkie-logo"
+				@click="logout"
+			/>
 		</div>
 	</div>
 	<div v-if="showModal" id="modal-overlay" ref="modalOverlay">
@@ -124,7 +147,9 @@ export default {
 					/>
 				</div>
 				<div class="modal-submit-container">
-					<button class="button" @click="editAccount(username, email, password)">Confirm Changes</button>
+					<button class="button" @click="editAccount(username, email, password)">
+						Confirm Changes
+					</button>
 				</div>
 			</div>
 			<div class="modal-container" v-if="del">
@@ -148,7 +173,9 @@ export default {
 					/>
 				</div>
 				<div class="modal-submit-container">
-					<button class="button" @click="deleteAccount(username, password)">Confirm Changes</button>
+					<button class="button" @click="deleteAccount(username, password)">
+						Confirm Changes
+					</button>
 				</div>
 			</div>
 		</div>
@@ -163,22 +190,23 @@ export default {
 	margin: 2rem 0rem;
 	padding: 1rem 4rem;
 	font-size: 1.25rem;
-  height: 5rem;
+	height: 5rem;
 }
 
 .nav-item-container {
 	display: flex;
 	flex-direction: row;
+	align-items: center;
 }
 
-.nav-icon{
-  height: 30px;
-  width: 30px;
+.nav-icon {
+	height: 30px;
+	width: 30px;
 }
 
 .nav-item {
 	font-weight: bold;
-  cursor: pointer;
+	cursor: pointer;
 }
 
 .item-left {
@@ -190,18 +218,18 @@ export default {
 }
 
 #modal-overlay {
-  position: absolute;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  top: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  background: rgba(0, 0, 0, 0.7);
-  display: flex;
-  align-items: center;
-  justify-content: center;
+	position: absolute;
+	left: 0;
+	right: 0;
+	bottom: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	background: rgba(0, 0, 0, 0.7);
+	display: flex;
+	align-items: center;
+	justify-content: center;
 }
 
 #modal {
